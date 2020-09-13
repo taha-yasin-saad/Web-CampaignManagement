c034fa9394e1190ec76dcb3ab6b2a3b9e421f564
ahmed775 <ahmed.kamal775@gmail.com>
2020-09-13 20:13:11 +0200

    Widget

--- app/Lead.php
+++ app/Lead.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lead extends Model
 {
+
+    protected $fillable = [
+        'product_id', 'name', 'email', 'phone', 'lead'
+    ];
+
     public function product(){
         return $this->belongsTo('\App\Product');
     }
