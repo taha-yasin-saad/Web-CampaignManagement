4049120e57a34ffd2fb92ae62d7b1c01eeb1a01c
mohamedismaiel12224 <mohamedismaiel12224@gmail.com>
2020-09-20 15:54:45 +0200

    push

--- app/Http/Controllers/WidgetController.php
+++ app/Http/Controllers/WidgetController.php
@@ -607,7 +607,7 @@ class WidgetController extends Controller
     public function widget_ajax(Request $request){
         $data = $request->all();
         unset($data['csrftoken']);
-        $user = Source::find($request->source_id)->workplace->users->withCount('leeds')->orderBy('leeds_count', 'asc')->first();
+        $user = Source::find($request->source_id)->workplace->users()->withCount('leads')->orderBy('leads_count', 'asc')->first();
         $save = new Lead;
         $save->source_id = $request->source_id;
         $save->user_id = $user->id;
