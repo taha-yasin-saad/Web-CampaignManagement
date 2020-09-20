7f8a9101081efba156ac76ec57d43ce6abe37bcd
kareem yahya <kareem@pharous.com>
2020-09-20 16:46:08 +0200

    k

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -54,7 +54,7 @@ class UserController extends Controller
         }
 
         $user = User::where('email',$request->email)->first();
-        if($user && $request->password == bcrypt($user['password'])){
+        if($user && Hash::make($request->password) == $user->password){
 			return response()->json(array(
                 'code' => 0,
                 'email' => $user->email,
