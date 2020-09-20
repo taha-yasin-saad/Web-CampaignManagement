492de2c4ff6dfcf5bd6cb0387106e202d3c64a4c
kareem yahya <kareem@pharous.com>
2020-09-20 16:10:08 +0200

    k

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -74,22 +74,21 @@ class UserController extends Controller
 
     protected function update_profile(Request $request)
     {
+        
         $data = $request->all();
+        $user = User::where('id', $request->id)->first();
         $rules = array(
                 'id'     => 'required',
                 'email'     => 'required',
                 'phone'     => 'required',
                 'name'      => 'required',
-            if ($user && $user->password == NULL ) {
-            'password'      => 'required',
-            }
+            if($user && $user->password == NULL){'password' => 'required'}
         );
         $validator = Validator::make($data, $rules);
         if ($validator->fails()) {
                 return response()->json(array('code' => 1,'msg_en'=> 'Wrong Data','msg_ar'=>'خطأ فى البيانات','error'=>$validator->messages()), 200, ['Access-Control-Allow-Origin' => '*'], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
         }
 
-        $user = User::where('id', $request->id)->first();
         if ($user && $user->password != NULL ) {
             $user->update([
                 'name' => $request->name,
