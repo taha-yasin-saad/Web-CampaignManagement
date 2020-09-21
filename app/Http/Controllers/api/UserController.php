ac8aafe67c365f101f63ff02ec767be0ad9993bd
kareem yahya <kareem@pharous.com>
2020-09-21 15:13:39 +0200

    k

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -50,9 +50,9 @@ class UserController extends Controller
             'email'     => 'required',
             'password'  => 'required',
         );
-        
+
         $validator = Validator::make($data, $rules);
-        
+
         if ($validator->fails()) {
                 return response()->json(array('code' => 1,'msg_en'=> 'Wrong Data','msg_ar'=>'خطأ فى البيانات','error'=>$validator->messages()), 200, ['Access-Control-Allow-Origin' => '*'], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
         }
@@ -84,7 +84,7 @@ class UserController extends Controller
                 return response()->json(array('code' => 1,'msg_en'=> 'Wrong Data','msg_ar'=>'خطأ فى البيانات','error'=>$validator->messages()), 200, ['Access-Control-Allow-Origin' => '*'], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
         }
 
-        $user = User::where('id', $request->id)->first();
+        $user = User::where('email', $request->email)->first();
         if ($user) {
             $user->update([
                 'name' => $request->name,
