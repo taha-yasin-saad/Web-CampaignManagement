d11984d0d7f0966cfbab47e534ba0450ec8506e0
mohamedismaiel12224 <mohamedismaiel12224@gmail.com>
2020-09-22 13:36:00 +0200

    pull

--- public/widget/widget.js
+++ public/widget/widget.js
@@ -587,7 +587,7 @@
         console.log(form_data);
 
         var xmlhttp = new XMLHttpRequest();
-        xmlhttp.open("POST", "widget", true);
+        xmlhttp.open("POST", "widgetView/widget", true);
         xmlhttp.setRequestHeader("Accept", "application/json");
         xmlhttp.setRequestHeader("x-csrf-token", document.getElementById("csrftoken").value);
         xmlhttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
