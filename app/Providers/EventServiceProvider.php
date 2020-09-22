9af7e388e7885b6803015ddcac1a9ce15f7f2d31
mohamedismaiel12224 <mohamedismaiel12224@gmail.com>
2020-09-22 13:56:04 +0200

    pull

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\NotificationEvent;
+use App\Listeners\SendNotificationListner;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -17,6 +19,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        NotificationEvent::class => [
+            SendNotificationListner::class,
         ],
     ];
 
