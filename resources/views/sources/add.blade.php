deab1e422ef618e62fe2b1a366d3fba6a5875cd5
MoFathy <mohammed.fathy.abdelrhman@gmail.com>
2020-11-11 20:13:19 +0200

    alignment

--- resources/views/sources/add.blade.php
+++ resources/views/sources/add.blade.php
@@ -270,7 +270,7 @@
                                                 <label style="margin-top:20px" id="submitt_label">Submitt Button Text</label><span
                                                     class="text-small text-muted"> (20 Characters Max)</span><br>
                                                 <input class="form-control" id="submitt_text" name="submitt_text" type="text"
-                                                    placeholder="Call Me Now">
+                                                    placeholder="Call Me Now" value="Call Me Now">
                                             </div>
                                         </div>
                                         <div class="col-md-4"
