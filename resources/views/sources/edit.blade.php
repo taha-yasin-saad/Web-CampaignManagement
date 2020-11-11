f71d5177e9bed32c543f9c93877831638709f13c
MoFathy <mohammed.fathy.abdelrhman@gmail.com>
2020-11-11 20:51:10 +0200

    edit widget

--- resources/views/sources/edit.blade.php
+++ resources/views/sources/edit.blade.php
@@ -382,8 +382,11 @@
 
 <script type="text/javascript">
 // $('.color').colorpicker({});
-// console.log('mmmmmmmmmmmmmmm');
-
+console.log({!!$data->widget_type!!});
+if({!!$data->widget_type!!} == 'text'){
+    $("#text-settings").show("swing");
+    $("#icon-settings").hide("swing");
+}
 $("#widget_type").change(function() {
     console.log('hhh');
     if (this.value == "text") {
