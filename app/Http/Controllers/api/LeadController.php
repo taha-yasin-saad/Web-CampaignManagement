b8004af9871df9af69e09e9d333599b28d376abb
ahmed775 <ahmed.kamal775@gmail.com>
2021-03-03 23:02:02 +0200

    Fix

--- app/Http/Controllers/api/LeadController.php
+++ app/Http/Controllers/api/LeadController.php
@@ -347,7 +347,7 @@ class LeadController extends Controller
         $save->product_id = $product_id;
         $save->name = $request->name;
         $save->email = $request->email;
-        if (preg_match('/(+20)/', $request->phone)) {
+        if (preg_match('/(20)/', $request->phone)) {
             $phone_num = ltrim($request->phone, '0');
         } else {
             $phone_num = $request->country_code .ltrim($request->phone, '0');
