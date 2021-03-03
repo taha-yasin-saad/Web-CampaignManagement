6e97abb34286783e32934589dc442c6c309095fa
ahmed775 <ahmed.kamal775@gmail.com>
2021-03-03 23:15:54 +0200

    country_code

--- app/Http/Controllers/api/LeadController.php
+++ app/Http/Controllers/api/LeadController.php
@@ -347,7 +347,7 @@ class LeadController extends Controller
         $save->product_id = $product_id;
         $save->name = $request->name;
         $save->email = $request->email;
-        if (preg_match('/(20)/', $request->phone)) {
+        if (preg_match('/(+20)/', $request->phone)) {
             $phone_num = $request->country_code . ltrim($request->phone, '+20');
         } else {
             $phone_num = $request->country_code . ltrim($request->phone, '0');
