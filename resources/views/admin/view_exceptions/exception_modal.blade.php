18cd828dd1b14acc93618c0c902a4db619858b84
ahmed775 <ahmed.kamal775@gmail.com>
2021-03-31 19:23:58 +0200

    exception_modal

--- resources/views/admin/view_exceptions/exception_modal.blade.php
+++ resources/views/admin/view_exceptions/exception_modal.blade.php
@@ -1,5 +1,6 @@
 <!-- Modal -->
-<div class="modal fade" id="exception_modal" tabindex="-1" role="dialog" aria-labelledby="exception_modalLabel" aria-hidden="true">
+<div class="modal fade" id="exception_modal" tabindex="-1" role="dialog" aria-labelledby="exception_modalLabel"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -9,18 +10,23 @@
                 </button>
             </div>
             <div class="modal-body">
-                <ul>
-                    <li id="url" style="font-weight: 500;padding: ">
-                        <p>Url :</p>
-                    </li>
-                    <li id="file" style="font-weight: 500;">
-                        <p>File :</p>
-                    </li>
-                    <li id="message" style="font-weight: 500;">
-                        <p>Message :</p>
-                    </li>
 
-                </ul>
+                <div class="card" >
+                    <div class="card-header" id="username" style="font-weight: 700;padding: 10px;">
+                        User Name :
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item" id="url" style="font-weight: 500;padding: 10px;word-wrap: break-word;">
+                            <p>Url :</p>
+                        </li>
+                        <li class="list-group-item" id="file" style="font-weight: 500;padding: 10px;word-wrap: break-word;">
+                            <p>File :</p>
+                        </li>
+                        <li class="list-group-item" id="message" style="font-weight: 500;padding: 10px;word-wrap: break-word;">
+                            <p>Message :</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
